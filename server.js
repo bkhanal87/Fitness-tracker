e9fd4d760de2f8ba578ae43c27a23f88266e8084
Basu Khanal <87610840+bkhanal87@users.noreply.github.com>
2021-11-29 18:43:15 -0500

    code added to server.js file

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const path = require("path");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(logger("dev"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+    userNewUrlParser: true,
+    useFindAndModify: false
+});
+
+app.use(require("./routes/htmlRoutes"));
+app.use(require("./routes/apiRoutes"));
+
+app.listen(PORT, () => {
+    console.log(`App running on port ${PORT}!`);
+});
