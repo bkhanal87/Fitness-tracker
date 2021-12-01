593dfa4921b63ab6ed11db195352bf12b87a3538
Basu Khanal <87610840+bkhanal87@users.noreply.github.com>
2021-11-30 23:08:40 -0500

    deploying

--- server.js
+++ server.js
@@ -14,13 +14,15 @@ app.use(express.json());
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
-    userNewUrlParser: true,
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/immense-crag-47322", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
     useFindAndModify: false
 });
 
-// app.use(require("./routes/htmlRoutes"));
-// app.use(require("./routes/apiRoutes"));
+app.use(require("./routes/htmlRoutes"));
+app.use(require("./routes/apiRoutes"));
 
 app.listen(PORT, () => {
     console.log(`App running on port ${PORT}!`);
